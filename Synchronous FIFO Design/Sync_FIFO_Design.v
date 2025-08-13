6c868d3ca918d380ec7d4aaf4b0da4c237935046
Vivek Samani <122169724+VIVEK-SAMANI@users.noreply.github.com>
2025-08-13 23:51:46 +0530

    Create Sync_FIFO_Design.v

---
+++ Synchronous FIFO Design/Sync_FIFO_Design.v
@@ -0,0 +1,41 @@
+`timescale 1ns / 1ps
+
+module synchronous_fifo #(parameter Depth = 8, Data_Width = 8) (
+  input clk, rst_n,
+  input w_en, r_en,
+  input [Data_Width-1:0] data_in,
+  output reg [Data_Width-1:0] data_out,
+  output full, empty
+);
+  
+  reg [$clog2(Depth)-1:0] w_ptr, r_ptr;
+  reg [Data_Width-1:0] fifo [Depth-1:0];
+  
+  // Set Default values on reset.
+  always@(posedge clk) begin
+    if(!rst_n) begin
+      w_ptr <= 0; r_ptr <= 0;
+      data_out <= 0;
+    end
+  end
+  
+  // Writing Block Logic
+  always@(posedge clk) begin
+    if(w_en & !full)begin
+      fifo[w_ptr] <= data_in;
+      w_ptr <= w_ptr + 1;
+    end
+  end
+  
+  // Reading Block Logic
+  always@(posedge clk) begin
+    if(r_en & !empty) begin
+      data_out <= fifo[r_ptr];
+      r_ptr <= r_ptr + 1;
+    end
+  end
+  
+  assign full = ((w_ptr+1'b1) == r_ptr);
+  assign empty = (w_ptr == r_ptr);
+  
+endmodule
